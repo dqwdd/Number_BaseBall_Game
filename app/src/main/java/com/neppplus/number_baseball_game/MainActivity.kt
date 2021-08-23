413ce64bd35f1a73aadead4b3926baa325225c12
keemaan <keemaan@naver.com>
2021-08-23 15:01:04 +0900

    어댑터 / 리스트뷰 연결

--- app/src/main/java/com/neppplus/number_baseball_game/MainActivity.kt
+++ app/src/main/java/com/neppplus/number_baseball_game/MainActivity.kt
@@ -2,11 +2,15 @@ package com.neppplus.number_baseball_game
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.number_baseball_game.adapters.MessageAdapter
 import com.neppplus.number_baseball_game.data.MessageData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val mMessagelist = ArrayList<MessageData>()
+
+    lateinit var mAdapter : MessageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +18,9 @@ class MainActivity : AppCompatActivity() {
 
         mMessagelist.add( MessageData("안녕하세요", "CPU") )
         mMessagelist.add( MessageData("반갑습니다", "USER") )
+
+        mAdapter = MessageAdapter(this, R.layout.message_list_item, mMessagelist)
+        messageListView.adapter = mAdapter
 
     }
 }
